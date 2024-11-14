5cdbaca9b535de17066e13ab2774a29e415f1848
schrieffer-z <schrieffer.zsy@gmail.com>
2024-11-14 14:14:59 +0000

    ...

--- alpaca_eval_from_outputs.py
+++ alpaca_eval_from_outputs.py
@@ -12,11 +12,12 @@ for fname in os.listdir( os.path.join(outputs_path, 'outputs') ):
         ed = fname.rfind('.')
         step_id = int(fname[st+1:ed])
         
-        os.makedirs(os.path.join(outputs_path, 'annotated_paired_outputs' ,f'step-{step_id}'),exist_ok=True)
+        output_path = os.path.join(outputs_path, 'annotated_paired_outputs' ,f'step-{step_id}')
+        os.makedirs(output_path, exist_ok=True)
         alpaca_eval.evaluate(
-            model_outputs=os.path.join(outputs_path, fname),
+            model_outputs=os.path.join(outputs_path, 'outputs', fname),
             reference_outputs=f'./evaluation/reference_models/pythia_{model_size}_post_sft.json',
             annotators_config='alpaca_eval_gpt4_turbo_fn', 
-            output_path=os.path.join(outputs_path, fname),
+            output_path=output_path,
             precomputed_leaderboard=os.path.join(outputs_path,'leaderboard.csv')
         )
