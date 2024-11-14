e2fbeea031e49a92455768af4fe859a3a0cd4171
schrieffer-z <schrieffer.zsy@gmail.com>
2024-11-14 14:12:39 +0000

    ...

---
+++ alpaca_eval_from_outputs.py
@@ -0,0 +1,22 @@
+import os
+import alpaca_eval
+
+
+
+model_size = '6.9b'
+outputs_path = f'./evaluation/pythia_{model_size}'
+
+for fname in os.listdir( os.path.join(outputs_path, 'outputs') ):
+    if fname[-5:]=='.json':
+        st = fname.rfind('_')
+        ed = fname.rfind('.')
+        step_id = int(fname[st+1:ed])
+        
+        os.makedirs(os.path.join(outputs_path, 'annotated_paired_outputs' ,f'step-{step_id}'),exist_ok=True)
+        alpaca_eval.evaluate(
+            model_outputs=os.path.join(outputs_path, fname),
+            reference_outputs=f'./evaluation/reference_models/pythia_{model_size}_post_sft.json',
+            annotators_config='alpaca_eval_gpt4_turbo_fn', 
+            output_path=os.path.join(outputs_path, fname),
+            precomputed_leaderboard=os.path.join(outputs_path,'leaderboard.csv')
+        )
